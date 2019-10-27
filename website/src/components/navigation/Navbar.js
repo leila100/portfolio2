1ad19d7d1ed509b5bb2cb9326e1ad8b0c98f79c5
Leila <nb.leila10@gmail.com>
2019-10-27 15:17:53 -0500

    Make the navbar fixed

--- website/src/components/navigation/Navbar.js
+++ website/src/components/navigation/Navbar.js
@@ -2,24 +2,26 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav class='navbar'>
-      <div class='name'>Leila Berrouayel</div>
-      <div class='links'>
-        <div class='item'>Projects</div>
-        <div class='item'>Resume</div>
-        <div class='icons'>
-          <a href='mailto:nb.leila10@gmail.com' target='_blank' rel='noopener noreferrer'>
-            <i className='far fa-envelope' />
-          </a>
-          <a href='https://www.linkedin.com/in/leila-berrouayel/' target='_blank' rel='noopener noreferrer'>
-            <i className='fab fa-linkedin' />
-          </a>
-          <a href='https://github.com/leila100' target='_blank' rel='noopener noreferrer'>
-            <i className='fab fa-github' />
-          </a>
+    <div class='fixed'>
+      <nav class='navbar'>
+        <div class='name'>Leila Berrouayel</div>
+        <div class='links'>
+          <div class='item'>Projects</div>
+          <div class='item'>Resume</div>
+          <div class='icons'>
+            <a href='mailto:nb.leila10@gmail.com' target='_blank' rel='noopener noreferrer'>
+              <i className='far fa-envelope' />
+            </a>
+            <a href='https://www.linkedin.com/in/leila-berrouayel/' target='_blank' rel='noopener noreferrer'>
+              <i className='fab fa-linkedin' />
+            </a>
+            <a href='https://github.com/leila100' target='_blank' rel='noopener noreferrer'>
+              <i className='fab fa-github' />
+            </a>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
