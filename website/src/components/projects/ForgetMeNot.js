4389caf76d391f2c5ac856bb33498391948476e3
Leila <nb.leila10@gmail.com>
2019-10-30 21:00:22 -0500

    Add the overview and team sections to fmn page

--- website/src/components/projects/ForgetMeNot.js
+++ website/src/components/projects/ForgetMeNot.js
@@ -30,6 +30,43 @@ const ForgetMeNot = () => {
           </div>
         </div>
       </header>
+      <section class='about'>
+        <h2>
+          <div>
+            <span>||</span> React<span>||</span> React Hooks<span>||</span>
+          </div>
+          <div>
+            Redux<span>||</span> Node<span>||</span>
+          </div>
+          <div>
+            sqlite3<span>||</span> Sendgrid<span>||</span> Full Calendar
+          </div>
+          <div>
+            <span>||</span> Styled Components<span>||</span> MaterialUI<span>||</span>
+          </div>
+        </h2>
+        <div class='descriptions'>
+          <div class='desc'>
+            <h3>Overview</h3>
+            <p>
+              Forget Me Not is an application that sends thoughtful messages, on your behalf, to people you know on
+              their special day.
+            </p>
+            <p>Added some technology (Redux for example), and refactored the code to create Forget Me Not.</p>
+          </div>
+          <div class='desc'>
+            <h3>Team</h3>
+            <p>
+              Built initially by a remote team of 5 for about 2 weeks. Was called BETTER FRIENDS REMINDERS (
+              <a href='https://friends-reminder.netlify.com/' target='_blank' rel='noopener noreferrer'>
+                Link to old app page
+              </a>
+              )
+            </p>
+            <p>Was responsible for the back end portion. Was also the team lead. </p>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
