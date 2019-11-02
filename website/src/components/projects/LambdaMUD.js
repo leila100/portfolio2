77dd829be61015f5e537bf2ce55014539c55b3fd
Leila <nb.leila10@gmail.com>
2019-11-02 13:22:53 -0500

    Add feature section to mud

--- website/src/components/projects/LambdaMUD.js
+++ website/src/components/projects/LambdaMUD.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 import mud from "../../assets/images/maze_avatar.png";
+import maze from "../../assets/images/maze.png";
+import directions from "../../assets/images/directions.png";
+import avatars from "../../assets/images/avatars.png";
+import chat1 from "../../assets/images/chat1.png";
+import chat2 from "../../assets/images/chat2.png";
 
 const LambdaMUD = () => {
   return (
@@ -59,6 +64,50 @@ const LambdaMUD = () => {
           <div class='desc'>
             <h3>Team</h3>
             <p>Built from scratch in less than two weeks along with four other developers in an Agile environment.</p>
+          </div>
+        </div>
+      </section>
+      <section class='features'>
+        <h2>
+          <span>||</span>FEATURES<span>||</span>
+        </h2>
+        <div class='feature'>
+          <h3>Maze</h3>
+          <p>
+            A maze is created allowing the user to have fun solving it by choosing a route to the exit door. To move,
+            the user can either use the arrows provided or use the W A S D keys on the keyboard.
+          </p>
+          <div class='imgs'>
+            <a href={maze} target='_blank' rel='noopener noreferrer'>
+              <img src={maze} alt='Maze' />
+            </a>
+            <a href={directions} target='_blank' rel='noopener noreferrer'>
+              <img src={directions} alt='directions to control avatar' />
+            </a>
+          </div>
+        </div>
+        <div class='feature'>
+          <h3>Choose your avatar</h3>
+          <p>A collection of user characters are available. Select the one you prefer or try them all.</p>
+          <div class='imgs'>
+            <a href={avatars} target='_blank' rel='noopener noreferrer'>
+              <img src={avatars} alt='Avatars' />
+            </a>
+            <a href={mud} target='_blank' rel='noopener noreferrer'>
+              <img src={mud} alt='maze' />
+            </a>
+          </div>
+        </div>
+        <div class='feature'>
+          <h3>Chat with other players</h3>
+          <p>Have a conversation with other players using the chatroom provided.</p>
+          <div class='imgs'>
+            <a href={chat1} target='_blank' rel='noopener noreferrer'>
+              <img src={chat1} alt='Chat room' />
+            </a>
+            <a href={chat2} target='_blank' rel='noopener noreferrer'>
+              <img src={chat2} alt='Chat room' />
+            </a>
           </div>
         </div>
       </section>
