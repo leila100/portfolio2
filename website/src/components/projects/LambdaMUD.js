534c886fdbdfcd7091bd5bc61e403fe5015d8ef6
Leila <nb.leila10@gmail.com>
2019-11-02 13:09:35 -0500

    Add overview section in mud

--- website/src/components/projects/LambdaMUD.js
+++ website/src/components/projects/LambdaMUD.js
@@ -30,6 +30,38 @@ const LambdaMUD = () => {
           </div>
         </div>
       </header>
+      <section class='about'>
+        <h2>
+          <div>
+            <span>||</span> React<span>||</span> Django<span>||</span>
+          </div>
+          <div>
+            sqlite3<span>||</span> Pusher
+          </div>
+          <div>
+            <span>||</span> StyledComponents<span>||</span> SASS<span>||</span>
+          </div>
+        </h2>
+        <div class='descriptions'>
+          <div class='desc'>
+            <h3>Overview</h3>
+            <p>
+              Lambda MUD is a multiplayer real-time world combining maze traversing, and online chat. Players select
+              their avatar, and then find their way to the door. Players can also interact with each other by using a
+              chatroom.
+            </p>
+            <p>
+              We designed, coded, and delivered a production level web application for people wanting to have fun
+              solving a maze.
+            </p>
+            <p>We focused on making a simple, user friendly application with an intuitive, and fun user experience.</p>
+          </div>
+          <div class='desc'>
+            <h3>Team</h3>
+            <p>Built from scratch in less than two weeks along with four other developers in an Agile environment.</p>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
