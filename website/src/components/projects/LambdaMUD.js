3156c833e262ace828321ba5a3eac77eefae8982
Leila <nb.leila10@gmail.com>
2020-01-17 17:23:28 -0600

    Add links for features and challenges in Lambda MUD header

--- website/src/components/projects/LambdaMUD.js
+++ website/src/components/projects/LambdaMUD.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-scroll";
 
 import mud_header from "../../assets/images/MUD_header.jpg";
 import mud from "../../assets/images/maze_avatar.png";
@@ -12,29 +13,39 @@ const LambdaMUD = () => {
   return (
     <>
       <header id='home'>
-        <div class='header'>
-          <img src={mud_header} alt='Maze' class='header_img' />
-          <div class='job_title'>Find your way!</div>
-          <div class='header_body'>
+        <div className='header'>
+          <img src={mud_header} alt='Maze' className='header_img' />
+          <div className='job_title'>Find your way!</div>
+          <div className='header_body'>
             <div>
-              <div class='name'>LAMBDA MUD</div>
-              <div class='links'>
-                <div class='link'>
+              <div className='name'>LAMBDA MUD</div>
+              <div className='links'>
+                <div className='link'>
                   <a href='https://lambda-mud7.netlify.com/' target='_blank' rel='noopener noreferrer'>
                     LINK
                   </a>
                 </div>
-                <div class='link'>
+                <div className='link'>
                   <a href='https://github.com/LambdaMUD' target='_blank' rel='noopener noreferrer'>
                     CODE
                   </a>
+                </div>
+                <div className='link'>
+                  <Link activeClass='activeProj' to='features' spy={true} smooth={true} offset={-100} duration={500}>
+                    Features
+                  </Link>
+                </div>
+                <div className='link'>
+                  <Link activeClass='activeProj' to='challenges' spy={true} smooth={true} offset={-70} duration={500}>
+                    Challenges
+                  </Link>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </header>
-      <section class='about'>
+      <section className='about'>
         <h2>
           <div>
             <span>||</span> React<span>||</span> Django<span>||</span>
@@ -46,8 +57,8 @@ const LambdaMUD = () => {
             <span>||</span> StyledComponents<span>||</span> SASS<span>||</span>
           </div>
         </h2>
-        <div class='descriptions'>
-          <div class='desc'>
+        <div className='descriptions'>
+          <div className='desc'>
             <h3>Overview</h3>
             <p>
               Lambda MUD is a multiplayer real-time world combining maze traversing, and online chat. Players select
@@ -60,23 +71,23 @@ const LambdaMUD = () => {
             </p>
             <p>We focused on making a simple, user friendly application with an intuitive, and fun user experience.</p>
           </div>
-          <div class='desc'>
+          <div className='desc'>
             <h3>Team</h3>
             <p>Built from scratch in less than two weeks along with four other developers in an Agile environment.</p>
           </div>
         </div>
       </section>
-      <section class='features'>
+      <section className='features' id='features'>
         <h2>
           <span>||</span>FEATURES<span>||</span>
         </h2>
-        <div class='feature'>
+        <div className='feature'>
           <h3>Maze</h3>
           <p>
             A maze is created allowing the user to have fun solving it by choosing a route to the exit door. To move,
             the user can either use the arrows provided or use the W A S D keys on the keyboard.
           </p>
-          <div class='imgs'>
+          <div className='imgs'>
             <a href={maze} target='_blank' rel='noopener noreferrer'>
               <img src={maze} alt='Maze' />
             </a>
@@ -85,10 +96,10 @@ const LambdaMUD = () => {
             </a>
           </div>
         </div>
-        <div class='feature'>
+        <div className='feature'>
           <h3>Choose your avatar</h3>
           <p>A collection of user characters are available. Select the one you prefer or try them all.</p>
-          <div class='imgs'>
+          <div className='imgs'>
             <a href={avatars} target='_blank' rel='noopener noreferrer'>
               <img src={avatars} alt='Avatars' />
             </a>
@@ -97,10 +108,10 @@ const LambdaMUD = () => {
             </a>
           </div>
         </div>
-        <div class='feature'>
+        <div className='feature'>
           <h3>Chat with other players</h3>
           <p>Have a conversation with other players using the chatroom provided.</p>
-          <div class='imgs'>
+          <div className='imgs'>
             <a href={chat1} target='_blank' rel='noopener noreferrer'>
               <img src={chat1} alt='Chat room' />
             </a>
@@ -110,12 +121,12 @@ const LambdaMUD = () => {
           </div>
         </div>
       </section>
-      <section class='about'>
+      <section className='about' id='challenges'>
         <h2>
           <span>||</span>CHALLENGES<span>||</span>
         </h2>
-        <div class='descriptions'>
-          <div class='desc'>
+        <div className='descriptions'>
+          <div className='desc'>
             <h3>Communication</h3>
             <p>
               Effective communication is the most important part of teamwork and involves consistently updating each
@@ -125,7 +136,7 @@ const LambdaMUD = () => {
               The team had communication problems that got resolved after feedback from team leads in Lambda School.
             </p>
           </div>
-          <div class='desc'>
+          <div className='desc'>
             <h3>Learning new Technology</h3>
             <p>
               Learning a new technology in a limited time is challenging, but we were required to use Django and Pusher.
@@ -135,7 +146,7 @@ const LambdaMUD = () => {
               - Implemented Pusher, on the last day of the project, to add chatting capabilities to the application.
             </p>
           </div>
-          <div class='desc'>
+          <div className='desc'>
             <h3>Division of work</h3>
             <p>Used a Trello board to divide and communicate the feature currently being built.</p>
             <p>Was responsible for coding the whole backend server.</p>
