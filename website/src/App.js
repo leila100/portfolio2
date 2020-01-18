2db000d734e14b8bb53ab97790387014f27e8430
Leila <nb.leila10@gmail.com>
2020-01-18 16:20:21 -0600

    Use AOS to animate the header in home page

--- website/src/App.js
+++ website/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 import Navbar from "./components/navigation/Navbar";
 import Home from "./components/home/Home";
 import HelloMelon from "./components/projects/HelloMelon";
 import ForgetMeNot from "./components/projects/ForgetMeNot";
 import LambdaMUD from "./components/projects/LambdaMUD";
+
+AOS.init();
 
 function App() {
   return (
--- website/src/components/home/header/Header.js
+++ website/src/components/home/header/Header.js
@@ -10,19 +10,21 @@ const Header = () => {
     <header id='home'>
       <div className='header'>
         <img src={header_image} alt='desk' className='header_img' />
-        <div className='job_title'>Web/Software Developer</div>
-        <div className='header_body'>
+        <div className='job_title' data-aos='fade-down' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
+          Web/Software Developer
+        </div>
+        <div className='header_body' data-aos='fade-zoom-in' data-aos-easing='ease-out-cubic' data-aos-duration='3000'>
           <div className='name'>
             <img src={leila} alt='Leila Berrouayel' className='leila_img' /> <div>Leila Berrouayel</div>
           </div>
           <div className='qualities'>Reliable . Committed . Dependable</div>
           <div className='links'>
-            <div className='link'>
+            <div className='link' data-aos='zoom-in' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
               <a href={resume} target='_blank' rel='noopener noreferrer'>
                 RESUME
               </a>
             </div>
-            <div className='link'>
+            <div className='link' data-aos='zoom-in' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
               <Link activeClass='activeProj' to='proj' spy={true} smooth={true} offset={-70} duration={500}>
                 Projects
               </Link>
