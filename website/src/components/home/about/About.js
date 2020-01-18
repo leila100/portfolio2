709f201148d253bf12f9288b0920f05de60bbd98
Leila <nb.leila10@gmail.com>
2020-01-18 16:30:50 -0600

    Use AOS to animate the about section in home page

--- website/src/components/home/about/About.js
+++ website/src/components/home/about/About.js
@@ -4,30 +4,32 @@ const About = () => {
   return (
     <section className='about'>
       <h2>
-        <div>
+        <div data-aos='flip-right' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
           <span>||</span>Web Developer<span>||</span>
         </div>
-        <div>React, Node, JavaScript, Python, CSS</div>
-        <div>
+        <div data-aos='flip-left' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
+          React, Node, JavaScript, Python, CSS
+        </div>
+        <div data-aos='flip-right' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
           <span>||</span>Agile Enthusiast<span>||</span>
         </div>
       </h2>
       <div className='descriptions'>
-        <div className='desc'>
+        <div className='desc' data-aos='zoom-in-right' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
           <h3>Web Developer</h3>
           <p>
             Joined Lambda School, an 18+ months Computer Science & Software Engineering online Academy that provides an
             immersive hands-on curriculum with a focus on computer science, software engineering, and web development.
           </p>
         </div>
-        <div className='desc'>
+        <div className='desc' data-aos='zoom-in-left' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
           <h3>Skills</h3>
           <p>
             Wrote production-ready code using ReactJS, Redux, and CSS on the front-end and NodeJS and Express, or Python
             on the back-end to build single page applications.
           </p>
         </div>
-        <div className='desc'>
+        <div className='desc' data-aos='fade-right' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
           <h3>Agile Methodology</h3>
           <p>
             Lambda School courses utilize the Agile methodology. They are all divided into sprints, with stand-ups
