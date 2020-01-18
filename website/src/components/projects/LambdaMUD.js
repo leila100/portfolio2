594cff829a1d0c902fbd5b95db42d3267fbab0d9
Leila <nb.leila10@gmail.com>
2020-01-18 17:32:46 -0600

    Use AOS to animate the Lambda MUD page

--- website/src/components/projects/LambdaMUD.js
+++ website/src/components/projects/LambdaMUD.js
@@ -19,27 +19,34 @@ const LambdaMUD = () => {
       <header id='home'>
         <div className='header'>
           <img src={mud_header} alt='Maze' className='header_img' />
-          <div className='job_title'>Find your way!</div>
-          <div className='header_body'>
+          <div className='job_title' data-aos='fade-down' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
+            Find your way!
+          </div>
+          <div
+            className='header_body'
+            data-aos='fade-zoom-in'
+            data-aos-easing='ease-out-cubic'
+            data-aos-duration='3000'
+          >
             <div>
               <div className='name'>LAMBDA MUD</div>
               <div className='links'>
-                <div className='link'>
+                <div className='link' data-aos='zoom-in' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
                   <a href='https://lambda-mud7.netlify.com/' target='_blank' rel='noopener noreferrer'>
                     LINK
                   </a>
                 </div>
-                <div className='link'>
+                <div className='link' data-aos='zoom-in' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
                   <a href='https://github.com/LambdaMUD' target='_blank' rel='noopener noreferrer'>
                     CODE
                   </a>
                 </div>
-                <div className='link'>
+                <div className='link' data-aos='zoom-in' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
                   <Link activeClass='activeProj' to='features' spy={true} smooth={true} offset={-100} duration={500}>
                     Features
                   </Link>
                 </div>
-                <div className='link'>
+                <div className='link' data-aos='zoom-in' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
                   <Link activeClass='activeProj' to='challenges' spy={true} smooth={true} offset={-70} duration={500}>
                     Challenges
                   </Link>
@@ -51,18 +58,18 @@ const LambdaMUD = () => {
       </header>
       <section className='about'>
         <h2>
-          <div>
+          <div data-aos='flip-right' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
             <span>||</span> React<span>||</span> Django<span>||</span>
           </div>
-          <div>
+          <div data-aos='flip-left' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
             sqlite3<span>||</span> Pusher
           </div>
-          <div>
+          <div data-aos='flip-right' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
             <span>||</span> StyledComponents<span>||</span> SASS<span>||</span>
           </div>
         </h2>
         <div className='descriptions'>
-          <div className='desc'>
+          <div className='desc' data-aos='fade-right' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
             <h3>Overview</h3>
             <p>
               Lambda MUD is a multiplayer real-time world combining maze traversing, and online chat. Players select
@@ -75,7 +82,7 @@ const LambdaMUD = () => {
             </p>
             <p>We focused on making a simple, user friendly application with an intuitive, and fun user experience.</p>
           </div>
-          <div className='desc'>
+          <div className='desc' data-aos='fade-left' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
             <h3>Team</h3>
             <p>Built from scratch in less than two weeks along with four other developers in an Agile environment.</p>
           </div>
@@ -83,10 +90,16 @@ const LambdaMUD = () => {
       </section>
       <section className='features' id='features'>
         <div className='title'>
-          <h2>
+          <h2 data-aos='flip-left' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
             <span>||</span>FEATURES<span>||</span>
           </h2>
-          <button className='top' onClick={scrollToTop}>
+          <button
+            className='top'
+            onClick={scrollToTop}
+            data-aos='zoom-in'
+            data-aos-easing='ease-out-cubic'
+            data-aos-duration='1500'
+          >
             Top
           </button>
         </div>
@@ -98,10 +111,22 @@ const LambdaMUD = () => {
           </p>
           <div className='imgs'>
             <a href={maze} target='_blank' rel='noopener noreferrer'>
-              <img src={maze} alt='Maze' />
+              <img
+                src={maze}
+                alt='Maze'
+                data-aos='flip-right'
+                data-aos-easing='ease-out-cubic'
+                data-aos-duration='1500'
+              />
             </a>
             <a href={directions} target='_blank' rel='noopener noreferrer'>
-              <img src={directions} alt='directions to control avatar' />
+              <img
+                src={directions}
+                alt='directions to control avatar'
+                data-aos='flip-left'
+                data-aos-easing='ease-out-cubic'
+                data-aos-duration='1500'
+              />
             </a>
           </div>
         </div>
@@ -110,10 +135,22 @@ const LambdaMUD = () => {
           <p>A collection of user characters are available. Select the one you prefer or try them all.</p>
           <div className='imgs'>
             <a href={avatars} target='_blank' rel='noopener noreferrer'>
-              <img src={avatars} alt='Avatars' />
+              <img
+                src={avatars}
+                alt='Avatars'
+                data-aos='flip-right'
+                data-aos-easing='ease-out-cubic'
+                data-aos-duration='1500'
+              />
             </a>
             <a href={mud} target='_blank' rel='noopener noreferrer'>
-              <img src={mud} alt='maze' />
+              <img
+                src={mud}
+                alt='maze'
+                data-aos='flip-left'
+                data-aos-easing='ease-out-cubic'
+                data-aos-duration='1500'
+              />
             </a>
           </div>
         </div>
@@ -122,25 +159,43 @@ const LambdaMUD = () => {
           <p>Have a conversation with other players using the chatroom provided.</p>
           <div className='imgs'>
             <a href={chat1} target='_blank' rel='noopener noreferrer'>
-              <img src={chat1} alt='Chat room' />
+              <img
+                src={chat1}
+                alt='Chat room'
+                data-aos='flip-right'
+                data-aos-easing='ease-out-cubic'
+                data-aos-duration='1500'
+              />
             </a>
             <a href={chat2} target='_blank' rel='noopener noreferrer'>
-              <img src={chat2} alt='Chat room' />
+              <img
+                src={chat2}
+                alt='Chat room'
+                data-aos='flip-left'
+                data-aos-easing='ease-out-cubic'
+                data-aos-duration='1500'
+              />
             </a>
           </div>
         </div>
       </section>
       <section className='about' id='challenges'>
         <div className='title'>
-          <h2>
+          <h2 data-aos='flip-left' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
             <span>||</span>CHALLENGES<span>||</span>
           </h2>
-          <button className='top' onClick={scrollToTop}>
+          <button
+            className='top'
+            onClick={scrollToTop}
+            data-aos='zoom-in'
+            data-aos-easing='ease-out-cubic'
+            data-aos-duration='1500'
+          >
             Top
           </button>{" "}
         </div>
         <div className='descriptions'>
-          <div className='desc'>
+          <div className='desc' data-aos='fade-right' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
             <h3>Communication</h3>
             <p>
               Effective communication is the most important part of teamwork and involves consistently updating each
@@ -150,7 +205,7 @@ const LambdaMUD = () => {
               The team had communication problems that got resolved after feedback from team leads in Lambda School.
             </p>
           </div>
-          <div className='desc'>
+          <div className='desc' data-aos='fade-left' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
             <h3>Learning new Technology</h3>
             <p>
               Learning a new technology in a limited time is challenging, but we were required to use Django and Pusher.
@@ -160,7 +215,7 @@ const LambdaMUD = () => {
               - Implemented Pusher, on the last day of the project, to add chatting capabilities to the application.
             </p>
           </div>
-          <div className='desc'>
+          <div className='desc' data-aos='fade-right' data-aos-easing='ease-out-cubic' data-aos-duration='1500'>
             <h3>Division of work</h3>
             <p>Used a Trello board to divide and communicate the feature currently being built.</p>
             <p>Was responsible for coding the whole backend server.</p>
